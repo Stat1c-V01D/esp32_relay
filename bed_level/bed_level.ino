81f8b757139d4e9c3ec043907d24e193d0909aad
St@t1c_V01D <kai.-schmitz@t-online.de>
2018-12-12 14:18:06 +0100

    initial commit

---
+++ bed_level/bed_level.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		bed_level.ino
+ Created:	06.12.2018 11:03:12
+ Author:	KS
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
